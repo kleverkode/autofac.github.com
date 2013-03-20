a51cba0c4b1f843ed65c8cd8817538328e0711c5
Travis Illig <tillig@paraesthesia.com>
2013-03-19 17:09:39 -0700

    Script generator complete.

--- js/scriptgen.js
+++ js/scriptgen.js
@@ -1,19 +1,42 @@
 (function($) {
 	"use strict";
 
-	var $form = $("#script_options");
+	var $form = $("#script_options"),
+		$output = $("#script_output");
 	$form.find("input:checkbox").click(generateScript);
 	$form.find("input:text").keyup(generateScript);
 
-	function generateScript()
-	{
-		var include = ["Autofac"];
-		$("#application_type,#library_integration,#additional_features").find("input:checkbox:checked").each(function()
-		{
-			include.push($(this).val());
+	function generateScript() {
+		var include = ["\"Autofac\""];
+		$("#application_type,#library_integration,#additional_features").find("input:checkbox:checked").each(function() {
+			include.push("\"" + $(this).val() + "\"");
 		});
-		// $packages = "Autofac", "Autofac.Mef"; foreach($package in $packages) { Install-Package $package; } Remove-Variable -Name "packages","package";
-		// Install-Package NAME [-ProjectName NAME] [-IncludePrerelease]
-		alert(include);
+		var script = "$aps=" + include.join() + ";foreach($ap in $aps){Install-Package $ap";
+		var projectName = $("#project_name").val().trim();
+		if (projectName) {
+			script += " -ProjectName " + projectName;
+		}
+		if ($("#include_prerelease").is(":checked")) {
+			script += " -IncludePrerelease";
+		}
+		script += ";}Remove-Variable -Name \"ap\",\"aps\";";
+		$output.text(script);
 	}
+
+	function selectScript() {
+		var range,
+		id = "script_output";
+		if (document.selection) {
+			range = document.body.createTextRange();
+			range.moveToElementText(id);
+			range.select();
+		} else if (window.getSelection) {
+			range = document.createRange();
+			range.selectNode(document.getElementById(id));
+			window.getSelection().addRange(range);
+		}
+	}
+
+	generateScript();
+	$("#select_all").click(selectScript);
 }(jQuery));
